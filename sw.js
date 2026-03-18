e3f061951402b4c3f374a9b3d7a22da1fa0c1dd8
recesar10 <recesar@gmail.com>
2026-03-17 22:12:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-
-const CACHE='treino-pro-v5';
-const ASSETS=['/treino/','/treino/index.html','/treino/manifest.json','/treino/icon-192.png','/treino/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const cp=n.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return n;}).catch(()=>caches.match('/treino/index.html'))));});
+self.addEventListener('fetch',()=>{});
