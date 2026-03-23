d96ef928a5d7052f5462ecf79a5d4e87d7f36786
recesar10 <recesar@gmail.com>
2026-03-23 10:58:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,49 @@
-const CACHE_NAME = "treino-proximo-auto-v1";
-const ASSETS = [
+const CACHE_NAME = "treino-semanal-series-v2";
+const URLS_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./animal_dragao.png",
-  "./animal_tigre.png",
-  "./animal_coelho.png",
-  "./animal_cavalo.png",
-  "./animal_cabra.png",
-  "./animal_serpente.png",
-  "./animal_macaco.png",
-  "./animal_galo.png",
-  "./animal_boi.png",
-  "./animal_porco.png",
-  "./animal_rato.png",
-  "./animal_cao.png"
+  "./manifest.json"
 ];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(fetch(e.request).then(r => {
-    const copy = r.clone();
-    caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
-    return r;
-  }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html"))));
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request)
+          .then((networkResponse) => {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return networkResponse;
+          })
+          .catch(() => caches.match("./index.html"))
+      );
+    })
+  );
 });
